bae474d3bcb404386f935c62c1bf82f57ebbbb94
Austin Jackson <themacphage@gmail.com>
2019-07-29 21:46:50 -0500

    draft import jsonl to firestore

---
+++ jsonl2firestore.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import os
+import os.path
+import sys
+import json
+import argparse
+import time
+from tqdm import tqdm
+from halo import Halo
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+parser = argparse.ArgumentParser(description='Import formatted JSONL files into Google Firestore')
+parser.add_argument('jsonlfiles', metavar='COSC 1430.jsonl', type=str, nargs='+',
+                    help='A set of CSV files to source data from')
+parser.add_argument('--key', dest='key', default=None,
+                    help='Path to Firebase Service account private key (see: README) ')
+
+args = parser.parse_args()
+#print(args)
+
+if args.key == None or not os.path.isfile(args.key):
+    print(f'{args.key} is not a file')
+    exit(1)
+
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
+spinner = Halo(text=f'Estimating number of entries to be processed ...', spinner='dots')
+spinner.start()
+
+
+sum = 0
+for arg in args.jsonlfiles:
+    sum += file_len(arg) # add line length (one row per line)
+    sum -= 1 # subtract header
+spinner.succeed(text=f'{sum} entries were counted in the provided .jsonl files')
+total_rows = sum
+
+# prepare Firebase, use a service account
+cred = credentials.Certificate(args.key)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+catalog = db.collection(u'catalog')
+
+with tqdm(total=total_rows, unit="rows") as t:
+    i = 1
+    for arg in args.jsonlfiles:
+        with open(arg, 'r') as f:
+            j = 0
+            sections = {}
+            for line in f:
+                obj = json.loads(line)
+                if j == 0:
+                    t.set_description(f'[{i}/{len(args.jsonlfiles)}] {obj["department"]} {obj["catalogNumber"]}')
+                    catalog.add(obj, document_id=f'{obj["department"]} {obj["catalogNumber"]}')
+                    sections = catalog.document(f'{obj["department"]} {obj["catalogNumber"]}').collection('sections')
+                else:
+                    sections.add(obj)
+                    t.update()
+                j += 1
+        i += 1
+
+exit(0)
+
